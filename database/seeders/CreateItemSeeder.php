650b81272009b1650000267d3c7d2e1c0c7ddd96
graciapurba09 <112602113+graciapurba09@users.noreply.github.com>
2024-10-31 14:00:37 +0700

    Update CreateItemSeeder.php

--- database/seeders/CreateItemSeeder.php
+++ database/seeders/CreateItemSeeder.php
@@ -12,6 +12,87 @@ class CreateItemSeeder extends Seeder
      */
     public function run(): void
     {
-        //
+        $Items = array(
+            array(
+                'item_id' => 'G01',
+                'item_name' => 'Guitar',
+                'location' => 'Storage A',
+                'descripton' => 'A musical instrument having a flat-backed round body, a long fretted neck, and usually six strings, played by plucking or strumming with the fingers or a plectrum.',
+                'item_status' => 'Available',
+                'condition' => 'Rusak',
+            ), 
+            array(
+                'item_id' => 'Bl12',
+                'item_name' => 'Basketball',
+                'location' => 'Storage B',
+                'descripton' => 'A game played between two teams of five players in which goals are scored by throwing a ball through a netted hoop fixed above each end of the court.',
+                'item_status' => 'Available',
+                'condition' => 'Bagus',
+            ),  
+            array(
+                'item_id' => 'Sht21',
+                'item_name' => 'Shuttlecock',
+                'location' => 'Storage B',
+                'descripton' => 'A light, cone-shaped object that is hit back and forth over a net in badminton and battledore, typically consisting of a cork that has been stripped of its feathers and attached to a small piece of cork or plastic.',
+                'item_status' => 'Available',
+                'condition' => 'Bagus',
+            ), 
+            array(
+                'item_id' => 'Vo21',
+                'item_name' => 'Vollyball',
+                'location' => 'Storage B',
+                'descripton' => 'A game for two teams in which the object is to keep a large ball in motion, from side to side over a high net, by striking it with the hands before it touches the ground.',
+                'item_status' => 'Available',
+                'condition' => 'Bagus',
+            ), 
+            array(
+                'item_id' => 'Tn32',
+                'item_name' => 'Table Tennis',
+                'location' => 'Storage B',
+                'descripton' => 'A game in which two or four players hit a small ball back and forth to each other with a bat, the object being to score 11 points before the opponent.',
+                'item_status' => 'Available',
+                'condition' => 'Bagus',
+            ),
+            array(
+                'item_id' => 'Ba22',
+                'item_name' => 'Bass',
+                'location' => 'Storage A',
+                'descripton' => 'A low-pitched instrument, the largest and lowest member of the violin family.',
+                'item_status' => 'Unavailable',
+                'condition' => 'Rusak',
+            ),
+            array(
+                'item_id' => 'K32',
+                'item_name' => 'Bass',
+                'location' => 'Storage A',
+                'descripton' => 'A low-pitched instrument, the largest and lowest member of the violin family.',
+                'item_status' => 'Unavailable',
+                'condition' => 'Rusak',
+            ),
+            array(
+                'item_id' => 'T11',
+                'item_name' => 'Tagading',
+                'location' => 'Storage A',
+                'descripton' => 'A traditional musical instrument from Indonesia.',
+                'item_status' => 'Unavailable',
+                'condition' => 'Good',
+            ),
+            array(
+                'item_id' => 'Ca23',
+                'item_name' => 'Camera',
+                'location' => 'Storage A',
+                'descripton' => 'A device for recording visual images in the form of photographs, film, or video signals.',
+                'item_status' => 'Available',
+                'condition' => 'Good',
+            ),
+            array(
+                'item_id' => 'Spk01',
+                'item_name' => 'Speaker',
+                'location' => 'Storage A',
+                'descripton' => 'A device that converts electrical signals into sound, typically as part of a public address system or stereo system.',
+                'item_status' => 'Available',
+                'condition' => 'Good',
+            ),
+        );
     }
 }
