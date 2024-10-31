8397678ebb1f463bac9fdca998c8e582c7c4fd34
Krisniasiahaan <112604450+Krisniasiahaan@users.noreply.github.com>
2024-10-31 14:19:15 +0700

    update

--- database/seeders/CreateFacilitySeeder.php
+++ database/seeders/CreateFacilitySeeder.php
@@ -14,6 +14,7 @@ return new class extends Seeder
     {
         $facilities = [
             [
+                'facility_id' => 1,
                 'name' => 'Ruang Serbaguna',
                 'detail' => 'Ruang yang dapat digunakan untuk berbagai keperluan seperti seminar, pertemuan, dan acara.',
                 'filename' => 'room1.jpg',
@@ -23,6 +24,7 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 3,
                 'name' => 'Kantin',
                 'detail' => 'Tempat makan yang menyediakan berbagai menu sehat dan cepat saji.',
                 'filename' => 'canteen.jpg',
@@ -32,6 +34,7 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 4,
                 'name' => 'Laboratorium Komputer',
                 'detail' => 'Laboratorium yang dilengkapi dengan komputer terbaru untuk keperluan pembelajaran.',
                 'filename' => 'computer_lab.jpg',
@@ -41,6 +44,7 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 5,
                 'name' => 'Lapangan Olahraga',
                 'detail' => 'Lapangan yang digunakan untuk berbagai jenis olahraga seperti sepak bola, basket, dan voli.',
                 'filename' => 'sports_field.jpg',
@@ -50,6 +54,7 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 6,
                 'name' => 'Ruang Meeting',
                 'detail' => 'Ruang yang dirancang khusus untuk pertemuan bisnis dan presentasi.',
                 'filename' => 'meeting_room.jpg',
@@ -59,6 +64,7 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 7,
                 'name' => 'Ruang Kelas',
                 'detail' => 'Kelas yang dilengkapi dengan kursi dan meja untuk kegiatan belajar mengajar.',
                 'filename' => 'classroom.jpg',
@@ -68,6 +74,7 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 8,
                 'name' => 'Perpustakaan',
                 'detail' => 'Tempat yang menyediakan berbagai koleksi buku dan sumber belajar lainnya.',
                 'filename' => 'library.jpg',
@@ -77,17 +84,19 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 9,
                 'name' => 'Open Teater',
-                'detail' => 'Ruangan untuk kumpul dan kegiatan',
-                'filename' => 'reading_room.jpg',
+                'detail' => 'Ruangan untuk kumpul dan kegiatan.',
+                'filename' => 'open_theater.jpg',
                 'status' => 'Accept',
                 'condition' => 'good',
                 'created_at' => now(),
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 10,
                 'name' => 'Auditorium',
-                'detail' => 'Ruang yang digunakan untuk kuliah umum',
+                'detail' => 'Ruang yang digunakan untuk kuliah umum.',
                 'filename' => 'auditorium.jpg',
                 'status' => 'Pending',
                 'condition' => 'good',
@@ -95,6 +104,7 @@ return new class extends Seeder
                 'updated_at' => now(),
             ],
             [
+                'facility_id' => 11,
                 'name' => 'Ruang Seni',
                 'detail' => 'Tempat untuk berkreasi dalam seni lukis dan kerajinan tangan.',
                 'filename' => 'art_room.jpg',
